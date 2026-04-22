a43d1e6b8571d70a17766df511dd519577eda30a
Meetpaghadar <142712312+Meetpaghadar@users.noreply.github.com>
2026-04-22 10:30:08 -0500

    Update 10285_acceptance_rate_by_date.sql

--- medium/10285_acceptance_rate_by_date.sql
+++ medium/10285_acceptance_rate_by_date.sql
@@ -12,14 +12,19 @@ If a request is not accepted, there is no record of it being accepted in the tab
 
 The output will only include dates where requests were sent and at least one of 
 them was accepted (acceptance can occur on any date after the request is sent).
-
-OUTPUT:
-- date
-- acceptance_rate
 */
+/*  not so good solution i thought i have to maintain user_id and sender_id*/--============================================================
+select fs.date,((count (case when fr.action = 'accepted' then 1  end))*1.0/
+(count(case when fs.action = 'sent' and fr.action = 'sent' then 1  end))) as percentage_acceptance
+from
+fb_friend_requests fs
+full join fb_friend_requests fr
+on fs.user_id_sender = fr.user_id_sender and 
+fs.user_id_receiver = fr.user_id_receiver
+where fs.action = 'sent' 
+group by fs.date
 
--- SOLUTION START --
-
+/*good one*/--============================================================================================================================
 WITH tmp AS (
     SELECT 
         user_id_sender, 
@@ -36,3 +41,14 @@ SELECT
     SUM(accepted_cnt) / SUM(sent_cnt) AS acceptance_rate
 FROM tmp
 GROUP BY date;
+/* Better and Compact solution */--===================================================================================================================
+SELECT 
+    date,
+    
+    COUNT(CASE WHEN action = 'accepted' THEN 1 END) * 1.0
+    / COUNT(CASE WHEN action = 'sent' THEN 1 END) AS acceptance_rate
+
+FROM fb_friend_requests
+
+GROUP BY date
+HAVING COUNT(CASE WHEN action = 'sent' THEN 1 END) > 0;
